5ec96057e6b2742d5781e59490f90810f64f01ee
Serge Ilyashenko <serj.ilyashenko@gmail.com>
2021-11-23 23:16:44 +0300

    get rid of babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
+import path from 'path'
 const NodemonPlugin = require('nodemon-webpack-plugin')
 
-module.exports = {
+export default {
   target: 'node',
   entry: './src/index.js',
   output: {
@@ -9,21 +9,5 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  plugins: [
-    new NodemonPlugin(), // Dong
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
+  plugins: [new NodemonPlugin()],
 }
